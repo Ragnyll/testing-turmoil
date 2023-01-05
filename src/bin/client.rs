91795cbf975a1741b98763d1c0b9329ef5953249
ragnyll <ragnyll@gallowzhumour.dev>
2023-01-05 15:50:57 -0600

    overengineered, but funny

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,17 @@
-fn main() {
+use std::{net::TcpStream, io::prelude::*};
 
+fn main() -> Result<(), std::io::Error> {
+    match TcpStream::connect("127.0.0.1:8000") {
+        Ok(mut stream) => {
+            let msg = b"lol what it do?";
+
+            stream.write(msg)?;
+        }
+        Err(err) => {
+            eprintln!("unable to connect");
+            return Err(err);
+        }
+    }
+
+    Ok(())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,59 +1,101 @@
 use std::net::TcpListener;
 
-fn handle_client<T: std::io::Read + std::io::Write>(mut stream: T) -> Result<(), std::io::Error> {
-    let mut bfr = [0_u8; 64];
-    loop {
-        match stream.read(&mut bfr) {
-            Ok(size) => {
-                let msg = &mut bfr[0..size];
-                stream.write(msg)?;
-                // return if the funciton is under test
-                #[cfg(test)]
-                {
-                    return Ok(());
-                }
-            }
-            Err(err) => {
-                println!("received error: {err}");
-                // return if the function is under test
-                #[cfg(test)]
-                {
-                    return Err(err);
-                }
-            }
+pub struct ConnectionHandler {
+    listener: Option<TcpListener>,
+}
+
+impl ConnectionHandler {
+    fn new(listener: TcpListener) -> Self {
+        Self {
+            listener: Some(listener),
         }
     }
 }
 
-/// Handle the incoming connections by assigning an indivdual worker to it.
-fn handle_incoming_connections(listener: &TcpListener) -> std::io::Result<()> {
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                std::thread::spawn(move || {
-                    // connection succeeded
-                    handle_client(stream)
-                });
+pub trait HandleClients<T: std::io::Read + std::io::Write>: HandleClient<T> {
+    fn handle_clients(&self) -> std::io::Result<()>;
+}
+
+pub trait HandleClient<T: std::io::Read + std::io::Write> {
+    fn handle_client(stream: T) -> Result<(), std::io::Error>;
+}
+
+// this trait should be integration tested
+impl<T: std::io::Read + std::io::Write> HandleClients<T> for ConnectionHandler {
+    fn handle_clients(&self) -> std::io::Result<()> {
+        match &self.listener {
+            Some(l) => {
+                for stream in l.incoming() {
+                    match stream {
+                        Ok(stream) => {
+                            println!("New connection: {}", stream.peer_addr().unwrap());
+                            std::thread::spawn(move || {
+                                // connection succeeded
+                                ConnectionHandler::handle_client(stream)
+                            });
+                        }
+                        Err(e) => {
+                            // connection failed
+                            println!("Error: {}", e);
+                        }
+                    }
+                }
             }
-            Err(e) => {
-                println!("Error: {}", e);
-                /* connection failed */
+            // This will only be hit in test. the new constructor requires that it be defined.
+            // Only a test will construct a
+            None => {
+                // define an error type type for this. the std::io::Result should not actually be
+                // used here
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Listener is not defined.",
+                ));
+            }
+        }
+
+        Ok(())
+    }
+}
+
+// This trait is unit tested
+impl<T: std::io::Read + std::io::Write> HandleClient<T> for ConnectionHandler {
+    fn handle_client(mut stream: T) -> Result<(), std::io::Error> {
+        let mut bfr = [0_u8; 64];
+        loop {
+            match stream.read(&mut bfr) {
+                Ok(size) => {
+                    let msg = &mut bfr[0..size];
+                    stream.write(msg)?;
+                    // return if the funciton is under test
+                    #[cfg(test)]
+                    {
+                        return Ok(());
+                    }
+                }
+                Err(err) => {
+                    println!("received error: {err}");
+                    // return if the function is under test
+                    #[cfg(test)]
+                    {
+                        return Err(err);
+                    }
+                }
             }
         }
     }
-
-    Ok(())
 }
 
 fn main() -> std::io::Result<()> {
     let addr = "127.0.0.1:8000";
     let listener = TcpListener::bind(addr)?;
+    let connection_handler = ConnectionHandler::new(listener);
 
     println!("Listening on address: {}", addr);
-    // I keep the loop becaue i want the user to know that this will indefinately run
+    // I keep the loop because i want the user to know that this will indefinately run
     loop {
-        handle_incoming_connections(&listener)?;
+        <ConnectionHandler as HandleClients<std::net::TcpStream>>::handle_clients(
+            &connection_handler,
+        )?;
     }
 }
 
@@ -82,8 +124,8 @@ mod test {
             }
         }
 
-        let mock_stream = StubTcpStream {};
-        assert_eq!(handle_client(mock_stream).unwrap(), ());
+        let stream_stub = StubTcpStream {};
+        assert_eq!(ConnectionHandler::handle_client(stream_stub).unwrap(), ());
     }
 
     #[test]
@@ -103,7 +145,7 @@ mod test {
             }
         }
 
-        let mock_stream = StubTcpStream {};
-        assert_eq!(handle_client(mock_stream).unwrap(), ());
+        let stream_stub = StubTcpStream {};
+        assert_eq!(ConnectionHandler::handle_client(stream_stub).unwrap(), ());
     }
 }
